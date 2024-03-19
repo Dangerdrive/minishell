136db7ee41eebdee58baf882eae3a5b1cb60b2cb
Graziela De Souza Antunes <gde-souz@c1r2p2.42sp.org.br>
2024-03-19 11:58:50 -0300

    added new quote tests

--- tests/tests.c
+++ tests/tests.c
@@ -3,21 +3,21 @@
 static t_global	*data = NULL;
 
 /*
-This function is called before each test case runs. 
-It's used to initialize data structures, allocate memory, 
-or set up any prerequisites necessary for the test to run under the expected conditions. 
+This function is called before each test case runs.
+It's used to initialize data structures, allocate memory,
+or set up any prerequisites necessary for the test to run under the expected conditions.
 Essentially, it prepares the test environment for each test.
 */
 void setUp(void) {
     data = init_data();
     }
 /*
-tearDown function: This function is called after each test case completes. 
+tearDown function: This function is called after each test case completes.
 Its primary purpose is to clean up the environment after a test has run.
- This might include freeing allocated memory, resetting hardware states, or clearing any modifications made during the test. 
+ This might include freeing allocated memory, resetting hardware states, or clearing any modifications made during the test.
 It helps ensure that one test case does not contaminate the environment for the next one, maintaining isolation between tests.
 */
-void tearDown(void) {     
+void tearDown(void) {
     if (data->cur_path != NULL) {
         free(data->cur_path);
     }
@@ -89,6 +89,20 @@ void test_handle_input_SuccessfullyHandlesInputWithQuotes(void) {
     ft_memdel(data->usr_input);
 }
 
+void test_handle_input_SuccessfullyHandlesInputWithOpenQuoteEnd(void) {
+    data->usr_input = strdup("Hello world\"");
+    handle_input(&data);
+    TEST_ASSERT_EQUAL_STRING("Error: open quote.\n", data->hashtable[0]->content);
+    ft_memdel(data->usr_input);
+}
+
+void test_handle_input_SuccessfullyHandlesInputWithOpenQuoteRandom(void) {
+    data->usr_input = strdup("\"He\"'llo 'worl'd");
+    handle_input(&data);
+    TEST_ASSERT_EQUAL_STRING("Error: open quote.\n", data->hashtable[0]->content);
+    ft_memdel(data->usr_input);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_copy_env_SuccessfullyCopiesEnvironmentVariables);
@@ -96,5 +110,7 @@ int main(void) {
     RUN_TEST(test_populate_hashtable_SuccessfullyAddsTokenToHashtable);
     RUN_TEST(test_handle_input_SuccessfullyHandlesSimpleInput);
     RUN_TEST(test_handle_input_SuccessfullyHandlesInputWithQuotes);
+    RUN_TEST(test_handle_input_SuccessfullyHandlesInputWithOpenQuoteEnd);
+    RUN_TEST(test_handle_input_SuccessfullyHandlesInputWithOpenQuoteRandom);
     return UNITY_END();
 }
