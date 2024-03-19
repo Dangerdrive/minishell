fa370e8b332082f7650204d3a51b3ca4de8151d5
Dangerdrive <felippealencar@gmail.com>
2024-03-19 19:21:54 -0300

    sort_env implemented. still need to clean and move ft_strarray_dup to libft

--- src/env.c
+++ src/env.c
@@ -15,17 +15,16 @@
 // 	free((*data)->env);
 // }
 
-void	print_our_env(t_global **data)
+void	print_env(char **env)
 {
 	int	i;
 
 	i = 0;
-	while ((*data)->env[i])
+	while (env[i])
 	{
-		printf("%s\n", (*data)->env[i]);
+		printf("%s\n", env[i]);
 		i++;
 	}
-//	free_env(data);
 }
 
 void	free_env(t_global **data)
@@ -70,39 +69,78 @@ int	copy_env(t_global **data)
 		i++;
 	}
 	(*data)->env[env_size] = NULL;
-	print_our_env(data);
 	return (EXIT_SUCCESS);
 }
 
-void		sort_env(t_global **data)
+char	**ft_strarray_dup(char **array)
 {
-	int		ordered;
 	int		i;
-	int		env_len;
-	char	*tmp;
+	char	**dup;
 
-	env_len = ft_strarray_len(data->env);
-	ordered = 0;
-	data->sorted_env = (char **)ft_calloc(env_len + 1, sizeof(char *));
-	//retomar daqui
-	while (tab && ordered == 0)
+	i = 0;
+	while (array[i])
+		i++;
+	dup = ft_calloc(1, (i + 1) * sizeof(char *));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (array[i])
 	{
-		ordered = 1;
-		i = 0;
-		while (i < env_len - 1)
+		dup[i] = ft_strdup(array[i]);
+		if (!dup[i])
 		{
-			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+			while (i > 0)
 			{
-				tmp = tab[i];
-				tab[i] = tab[i + 1];
-				tab[i + 1] = tmp;
-				ordered = 0;
+				i--;
+				free(dup[i]);
 			}
-			i++;
+			free(dup);
+			return (NULL);
 		}
-		env_len--;
+		i++;
 	}
+	dup[i] = NULL;
+	return (dup);
+}	
+
+// "export" shows environment variables in alphabetical order, but it has 1 line less than envp
+void sort_env(t_global **data)
+{
+    int ordered;
+    int i;
+    int env_len;
+    char *tmp;
+
+    env_len = ft_strarray_len((*data)->env); // Corrected access to data's members
+    ordered = 0;
+	(*data)->sorted_env = ft_strarray_dup((*data)->env);
+    // (*data)->sorted_env = (char **)ft_calloc(env_len + 1, sizeof(char *));
+    // if (!(*data)->sorted_env)
+    //     return;
+    // i = 0;
+    // while (i < env_len) {
+    //     (*data)->sorted_env[i] = ft_strdup((*data)->env[i]); // Corrected access
+    //     i++;
+    // }
+    // (*data)->sorted_env[env_len] = NULL;
+    while (!ordered)
+	{
+        ordered = 1;
+        i = 0;
+        while (i < env_len - 1) {
+            if (ft_strcmp((*data)->sorted_env[i], (*data)->sorted_env[i + 1]) > 0) {
+                tmp = (*data)->sorted_env[i];
+                (*data)->sorted_env[i] = (*data)->sorted_env[i + 1];
+                (*data)->sorted_env[i + 1] = tmp;
+                ordered = 0;
+            }
+            i++;
+        }
+        env_len--; // This line is placed correctly; it gradually reduces the array's size being sorted.
+    }
+
 }
+
 
 //    data->sorted_env = (char **)malloc(sizeof(char *) * (env_len + 1));
 //     for (i = 0; i < env_len; i++) {
@@ -126,12 +164,14 @@ void		sort_env(t_global **data)
 //     }
 // }
 
-// int	main(void)
-// {
-// 	static t_global	*data;
+int	main(void)
+{
+	static t_global	*data;
 
-// 	data = init_data();
-// 	copy_env(&data);
-// 	free(data);
-// 	free(data->cur_path);
-// }
+	data = init_data();
+	copy_env(&data);
+	sort_env(&data);
+	print_env(data->sorted_env);
+	free(data);
+	free(data->cur_path);
+}
