b6163de33be38b4c594dab262df6ebf3c8800d73
Dangerdrive <felippealencar@gmail.com>
2024-04-26 14:25:58 -0300

    - Commit on 2024.04.26\t14h25m57

---
+++ libs/libft/ft_strarr_stradd.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarr_stradd.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-souz <gde-souz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 20:52:11 by fde-alen          #+#    #+#             */
+/*   Updated: 2024/04/09 15:25:37 by gde-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char **ft_strarr_dup_extend(char **array, int len, const char *new_str)
+{
+	char	**new_array;
+	int		i;
+
+	new_array = (char **)malloc(sizeof(char *) * (len + 2));
+	if (!new_array)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_array[i] = ft_strdup(array[i]);
+		if (!new_array[i])
+		{
+			ft_strarr_free(new_array, i);
+			return (NULL);
+		}
+		i++;
+	}
+	new_array[i] = ft_strdup(new_str);
+	// new_array[i] = ft_calloc(ft_strlen(new_str), sizeof(char));
+	// ft_strlcpy(new_array[i], new_str, ft_strlen(new_str));
+	new_array[++i] = NULL;
+	return (new_array);
+}
+
+// Function to add a string to a string array.
+void	ft_strarr_stradd(char ***strarr, const char *str)
+{
+	int		len;
+	char 	**new_arr;
+
+	if (!str)
+		return ;
+	len = ft_strarr_len(*strarr);
+	new_arr = ft_strarr_dup_extend(*strarr, len, str);
+	if (!new_arr)
+		return ;
+	ft_strarr_free(*strarr, len);
+	*strarr = new_arr;
+}
---
+++ src/builtins/cd.c
@@ -0,0 +1,105 @@
+#include "../../includes/minishell.h"
+
+static void	replace_or_add_env(char *arg, t_global *data)
+{
+	char	*key;
+
+	key = NULL;
+	if (ft_strchr_i(arg, '=') != -1)
+	{
+		key = ft_strndup(arg, ft_strchr_i(arg, '='));
+		ft_strarr_str_replace(data->env, key, arg);
+	}
+	else if (ft_strchr_i(arg, '=') == -1)
+		key = ft_strdup(arg);
+	if (ft_strarr_str(data->env, key) == -1
+		&& ft_strarr_str(data->env, key) == -1)
+		ft_strarr_stradd(&data->env, arg);
+	if (key != NULL)
+		free(key);
+}
+
+static int	update_pwds(t_global *data, int option)
+{
+	char	cwd[4096];
+	char	*pwd;
+
+	if (getcwd(cwd, 4096) == NULL)
+		return (1);
+	if (option == 1)
+		pwd = ft_strjoin("OLDPWD=", cwd);
+	else
+		pwd = ft_strjoin("PWD=", cwd);
+	if (!pwd)
+		return (1);
+	replace_or_add_env(pwd, data);
+	ft_memdel(pwd);
+	return (0);
+}
+
+static int	go_oldpwd(t_global *data)
+{
+	int		result;
+	char	*path;
+	char	*old_path;
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	old_path = ft_strjoin("OLDPWD=", cwd);
+	path = NULL;
+	path = ft_getenv("OLDPWD", &data);
+	if (!path)
+	{
+		ft_dprintf(STDERR_FILENO, "minishell : cd: OLDPWD not set\n");
+		return (0);
+	}
+	result = chdir(path);
+	replace_or_add_env(old_path, data);
+	ft_memdel(old_path);
+	ft_memdel(cwd);
+	return (result);
+}
+
+static int	go_home(t_global *data)
+{
+	int		result;
+	char	*path;
+
+	path = NULL;
+	update_pwds(data, 1);
+	path = ft_getenv("HOME", &data);
+	if (!path)
+		ft_dprintf(STDERR_FILENO, "minishell: cd: HOME not set\n");
+	if (!path)
+		return (0);
+	result = chdir(path);
+	return (result);
+}
+
+int	ft_cd(char **args, int args_len, t_global *data)
+{
+	int		result;
+
+	if (args_len > 2)
+	{
+		result = 1;
+		ft_dprintf(2, "minishell : cd: too many arguments");
+	}	
+	else if (args_len == 1)
+		result = go_home(data);
+	else if (ft_strcmp(args[1], "-") == 0)
+		result = go_oldpwd(data);
+	else
+	{
+		update_pwds(data, 1);
+		result = chdir(args[1]);
+		if (result < 0)
+			result *= -1;
+		if (result != 0)
+			ft_dprintf(2, "minishell: cd: %s: %s\n", args[1], strerror(errno));
+	}
+	update_pwds(data, 0);
+	return (result);
+}
+
+	//printf("\t%s\n", data->env[ft_strarr_str(data->env, "OLDPWD")]);
