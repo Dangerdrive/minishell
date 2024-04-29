6e760c8dc58779c905855e0b4128732ff0807537
Graziela De Souza Antunes <gde-souz@c1r6p4.42sp.org.br>
2024-04-29 14:41:42 -0300

    empty quotes are being properly handled

--- src/execution/exec_utils.c
+++ src/execution/exec_utils.c
@@ -17,23 +17,41 @@ int	hashsize(t_tkn *hashtable)
 	return (i);
 }
 
+t_bool	check_empty_str(t_tkn **node)
+{
+	if ((*node)->prev && ((*node)->prev->content
+			&& !ft_strcmp((*node)->prev->content, "")
+			&& (*node)->prev->space_after == TRUE)
+		&& (*node)->prev->prev && (*node)->prev->prev->space_after == TRUE)
+		return (TRUE);
+	if ((*node)->content && !ft_strcmp((*node)->content, "")
+		&& (*node)->prev->space_after == TRUE)
+		return (TRUE);
+	return (FALSE);
+}
+
 // REVISAR SITUAÇÃO STRING VAZIA (echo lala"" haha)
 void	fill_args(t_tkn	**node, char **args, int i)
 {
 	char	*arg_tmp;
 
 	arg_tmp = NULL;
-	if ((*node)->prev && ((*node)->prev->content
-			&& !ft_strcmp((*node)->prev->content, "")
-			&& (*node)->prev->space_after == TRUE))
+	if (check_empty_str(node))
 		args[i] = ft_strjoin(" ", (*node)->content);
-	else if ((*node)->space_after == TRUE && (*node)->content)
+	if (!(*node)->prev
+		|| ((*node)->prev && ((*node)->prev->space_after == TRUE)
+		&& (*node)->content))
 		args[i] = ft_strdup((*node)->content);
-	if ((*node)->space_after == FALSE && (*node)->next)
+	if (((*node)->space_after == FALSE) && (*node)->next)
 	{
-		if ((*node)->prev->space_after == TRUE)
-			args[i] = ft_strdup((*node)->content);
-		arg_tmp = ft_strjoin(args[i], (*node)->next->content);
+		if (!ft_strcmp((*node)->next->content, "") && (*node)->next->next
+			&& (*node)->next->space_after == FALSE)
+		{
+			arg_tmp = ft_strjoin(args[i], (*node)->next->next->content);
+			*node = (*node)->next;
+		}
+		else
+			arg_tmp = ft_strjoin(args[i], (*node)->next->content);
 		free(args[i]);
 		args[i] = arg_tmp;
 		(*node) = (*node)->next;
